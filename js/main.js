fd987f20a294ae584f054d6174319bd959e8d16f
Adam Reszka <adam.reszka85@gmail.com>
2018-07-26 19:17:15 +0200

    add js structure

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(initAll);
+
+function initAll() {
+  atfHeight();
+  // if ($(window).width() > 980) {
+  //   rellax();
+  // }
+}
+
+function atfHeight() {
+  // $('#header-section').height($(window).innerHeight());
+}
