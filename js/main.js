c19afc4f7d4734a5f9407434c744bb4f4569e84e
AdamReszka <adam.reszka85@gmail.com>
2018-08-09 21:45:37 +0200

    add final version of js scrypt

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 $(document).ready(startMe);
+var di = 0; //decoding iterator
+var gi = 0; //global iterator
+var valueLenght;
+var valuesLenght;
 
 function startMe() {
   setHeight();
-  drawChar();
+  drawChar(gi);
 }
 
 function setHeight() {
   $('#sec1-atf-cont').height($(window).innerHeight());
 }
 
-function drawChar() {
+function drawChar(gi) {
+  di = 0;
   var isDrawing = true;
   var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var values = ["foxivity","progressivity","expressivity","creativity"];
+  var values = ["progressivity","expressivity","creativity","foxivity"];
   var random;
-
-
-  var spellValue = [];
-  for (i=0;i<values.length;i++) {
-    var value = values[i];
-    var valueLenght = value.length;
+  var gcheck = gi - 1;
+  valuesLenght = values.length;
+  spellValue = [];
+    var value = values[gi];
+    valueLenght = value.length;
     for (j=0;j<valueLenght;j++) {
       spellValue.push(value.charAt(j));
-      $('.decoding-phrase').append('<span class="single-char ondraw char' + j + '">x<span>');
+      $('.decoding-phrase').append('<span class="single-char fox' + gi + ' ondraw char' + j + '">x<span>');
+      var valueParam = valuesLenght - 1;
+      if ($('.single-char').hasClass('fox'+valueParam)) {
+        $('.single-char').each(function() {
+          $(this).addClass('foxed');
+        });
+      }
       function drawAgain() {
         $('.single-char').each(function() {
           if(isDrawing == true && $(this).hasClass('ondraw')) {
@@ -32,47 +42,44 @@ function drawChar() {
         });
         var timeout1 = window.setTimeout(function() {
           drawAgain();
-        }, 100);
+        }, 90);
       }
       drawAgain();
-      // setTimeout(function() {
-      //   $('.single-char').each(function() {
-      //     $(this).remove();
-      //   });
-      // }, 10000);
     }
-
-    // console.log(value);
-    // console.log(spellValue);
+  if (gi == 0) {
+    var timeout4 = window.setTimeout(function() {
+      for (k=0;k<valueLenght;k++) {
+        decodeMe(k);
+      }
+    }, 1000);
+  } else {
+    var timeout5 = window.setTimeout(function() {
+      for (k=0;k<valueLenght;k++) {
+        decodeMe(k);
+      }
+    }, 700);
   }
-  for (k=0;k<valueLenght;k++) {
-    setDelay(k);
-  }
-  function setDelay(k) {
-    console.log('yes we test '+k);
+  function decodeMe(k) {
     var currentChar = '.char'+k;
     var timeout2 = window.setTimeout(function() {
       $(currentChar).removeClass('ondraw').html(spellValue[k]);
-    }, 300 * (k+1));
+      di++;
+      if(di == valueLenght) {
+        gi++;
+        var timeout3 = window.setTimeout(function() {
+          if (gi<valuesLenght) {
+            $('.single-char').each(function() {
+              $(this).remove();
+            });
+          } else {
+            $('.extention').addClass('visible');
+          }
+          drawChar(gi);
+        }, 500);
+      }
+    }, 180 * (k+1));
   }
   $('#drawing-stop').on('click', function() {
     isDrawing = false;
   });
-
-  function qwe(l) {
-    console.log(examp);
-    console.log(examp[l]);
-    console.log('adam');
-  }
-
-  var examp = [];
-  for(l=0;l<10;l++) {
-    var name = "name"+l;
-    examp.push(name);
-
-    setTimeout(qwe(l), 1000);
-  }
-  setTimeout(function() {
-    console.log('qwerasd');
-  },5000);
 }
